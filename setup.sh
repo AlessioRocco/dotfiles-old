816d00d81167264cbbd3957d9561472d5b31a300
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-09-20 13:20:01 +0200

    Remove setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles
-env RCRC=$HOME/dotfiles/rcrc rcup
-git clone git://github.com/AlessioRocco/dotfiles ~/dotfiles-local
-rcup
-curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
-sh mac 2>&1
